8df72986e78fef85f708559295dd51573d7e1eb7
Sampathkumar-s <sampathkumarsks12@gmail.com>
2025-12-15 14:48:29 +0530

    Multiple object created

--- example2.js
+++ example2.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3009;
-const data = {
+const data = [{
     "name": "darkprince",
     "passwd": "darkprince123",
     "city": "Erode",
     "age": 21,
     "phone": 1234567890
-};
+},
+{
+    "name": "sampath",
+    "passwd": "sampath123",
+    "city": "Erode",
+    "age": 21,
+    "phone": 1234567890
+}];
 
 app.get("/", (req, res) => {
   res.json({"msg":"user data fetch successfully", data});
