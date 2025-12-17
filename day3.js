a5785c1955f344faa56fc6c084fb1d0b2fd311b9
Sampathkumar-s <sampathkumarsks12@gmail.com>
2025-12-17 11:08:20 +0530

    get methoad is added

--- day3.js
+++ day3.js
@@ -6,16 +6,19 @@ const router = express.Router();
 
 app.use(express.json());
 
+// MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/kongu")
-.then(() => console.log("MongoDB is connected"))
+.then(() => console.log("MongoDB connected"))
 .catch(err => console.log("MongoDB error:", err));
 
+// Schema
 const dataSchema = new mongoose.Schema({
     name: String,
     age: Number,
     city: String
 });
 
+// Model
 const Person = mongoose.model("Data", dataSchema);
 
 // POST API
@@ -25,13 +28,39 @@ router.post("/data", async (req, res) => {
         const savedData = await newData.save();
         res.status(201).json(savedData);
     } catch (err) {
-        res.status(400).json({ message: err.message });
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// GET ALL DATA
+router.get("/getdata", async (req, res) => {
+    try {
+        const savedData = await Person.find();
+        res.status(200).json(savedData);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// GET BY NAME
+router.get("/data/:name", async (req, res) => {
+    try {
+        const savedData = await Person.findOne({ name: req.params.name });
+
+        if (!savedData) {
+            return res.status(404).json({ message: "Data not found" });
+        }
+
+        res.status(200).json(savedData);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
 // Connect router
 app.use("/api", router);
 
+// Server
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
