7ce0e6b8017362b0787ef118aeb976e3e63c7aab
Sampathkumar-s <sampathkumarsks12@gmail.com>
2025-12-17 12:10:57 +0530

    Update code added

--- day3.js
+++ day3.js
@@ -77,6 +77,28 @@ router.delete("/data/:name", async (req, res) => {
     }
 });
 
+
+router.put("/data/:name", async (req, res) => {
+    try {
+        const updatedData = await Person.findOneAndUpdate(
+            { name: req.params.name },   // condition
+            req.body,                    // new data
+            { new: true }                // return updated document
+        );
+
+        if (!updatedData) {
+            return res.status(404).json({ message: "Data not found" });
+        }
+
+        res.status(200).json({
+            message: "Data updated successfully",
+            updatedData
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 // Connect router
 app.use("/api", router);
 
